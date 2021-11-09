6112b5354712effed9c0d7fd02ce812b44d5cc4a
Mustafa Aytunç <mustafaaytu@gmail.com>
2021-11-10 01:17:59 +0300

    survey model's association added.

--- app/models/survey.rb
+++ app/models/survey.rb
@@ -1,2 +1,16 @@
+# frozen_string_literal: true
+
+# == Schema Information
+#
+# Table name: surveys
+#
+#  id         :uuid             not null, primary key
+#  name       :text
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class Survey < ApplicationRecord
+  has_many :questions, dependent: :destroy
+  has_many :feedbacks, dependent: :destroy
+  has_many :responses, through: :questions, dependent: :destroy
 end
