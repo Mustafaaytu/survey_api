7b089cc07a8578b35c1c74eba3a612cb3b736323
Mustafa Aytunç <mustafaaytu@gmail.com>
2021-11-10 15:35:47 +0300

    feedback model tests were wrote.

--- test/models/feedback_test.rb
+++ test/models/feedback_test.rb
@@ -14,7 +14,18 @@
 require "test_helper"
 
 class FeedbackTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  context 'database scheme' do
+    should have_db_column(:id).of_type(:uuid).with_options(null: false)
+    should have_db_column(:survey_id).of_type(:uuid)
+    should have_db_column(:created_at).of_type(:datetime).with_options(null: false)
+    should have_db_column(:updated_at).of_type(:datetime).with_options(null: false)
+    should have_db_index(:survey_id)
+  end
+
+  context 'associations' do
+    should belong_to(:survey)
+  end
+
+  context 'attributes' do
+  end
 end
